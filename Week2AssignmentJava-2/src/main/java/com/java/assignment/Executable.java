658bc6bbcdc70f4126cf9e9b4d200a4537347be5
Dhruv Agarwal <dhruv.23101998@gmail.com>
2020-08-25 00:15:08 +0530

    Committing Week2 Assignment

--- Week2AssignmentJava-2/src/main/java/com/java/assignment/Executable.java
+++ Week2AssignmentJava-2/src/main/java/com/java/assignment/Executable.java
@@ -6,6 +6,9 @@ public class Executable {
 	public static void main(String[] args) throws IOException {
 		ProcessTransactions pt=new ProcessTransactions("TransactionHistory.csv");
 		pt.calculateProcessingFee();
-		pt.createSummaryReport("SummaryReport.csv");
+		pt.createSummaryReport("SummaryReport-TransactionHistory.csv");
+		ProcessTransactions ptn=new ProcessTransactions("Sample_Data_Fee_Calculator.csv");
+		ptn.calculateProcessingFee();
+		ptn.createSummaryReport("SummaryReport-Sample_Data_Fee_Calculator.csv");
 	}
 }
